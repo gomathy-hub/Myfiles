d041383afa82890b73922b89b7581d1ae928e1bd
gomathy-hub <68999612+gomathy-hub@users.noreply.github.com>
2020-07-30 18:38:03 +0530

    Sample Project

---
+++ SampleApp.vue
@@ -0,0 +1,53 @@
+<template>
+<v-layout row wrap>
+  <Card :datas="results"/>
+</v-layout>
+  
+</template>
+<script>
+import axios from "axios";
+import Card from "./Card"
+
+export default {
+  components:{
+    Card
+  },
+  data() {
+    return {
+      results: [],
+      
+    };
+  },
+  mounted() {
+   
+    axios({
+      method: "GET",
+      url:
+        "https://jsonplaceholder.typicode.com/todos/"
+    }).then(
+      result => {
+        this.results = result.data;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
+    
